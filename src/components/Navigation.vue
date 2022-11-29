1996069a84858b45518d0513b4667927a5cec2c3
LaurenV <laurenvdl45@gmail.com>
2022-11-29 13:25:07 +0100

    feat: create component for the navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <img src="./../assets/donuttello-logo.png" alt="donutello">
+    <a class="nav--link" href="/">Home</a>
+    <a class="nav--link" href="/">Configurator</a>
+    <a class="nav--link" href="/gallery.html">Gallerij</a>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    margin: 0;
+    display: flex;
+    padding: 2rem;
+    justify-content: right;
+    align-items: center;
+    background-color: #E72C70;
+  }
+
+  .nav--link {
+    margin-right: 2rem;
+    color: #fff;
+  }
+
+  img {
+    margin-top: -3rem;
+    margin-right: auto;
+  }
+  
+</style>
