d97901e45228ade9a57faf434ed8ad4b00837595
LaurenV <laurenvdl45@gmail.com>
2022-12-11 16:08:15 +0100

    style: change css on home and nav

--- public/views/Home.vue
+++ public/views/Home.vue
@@ -5,10 +5,11 @@ import Navigation from '../../src/components/Navigation.vue';
 </script>
 
 <template>
-  <div>
     <Navigation />
-    <h1>Home</h1>
-    <p>Welkom op de homepagina van Donutello</p>
+    <div class="image">
+    <h1 class="title">De <span class="title title--highlight">donut</span> configurator!</h1>
+    <p>Wil jij ook je werknemers graag eens verrassen met een donut? Via onze 3D donut configurator kan je je eigen donut maken!</p>
+    <a class="btn" href="https://development5-eindbaas-three.vercel.app/">creeër donut</a>
   </div>
 </template>
 
@@ -16,5 +17,41 @@ import Navigation from '../../src/components/Navigation.vue';
   p {
     margin: 0;
     padding: 2rem;
+    font-size: 1.5em;
+    color: white;
+    font-weight: bolder;
   };
+
+  .title {
+    color: white;
+    font-weight: bold;
+  }
+
+  .title--highlight {
+    color: var(--strawberry);
+  }
+
+  .btn {
+    background-color: #f7e200;
+    color: #E72870;
+    font-size: clamp(0.9rem, 1vw + 1rem, 1.5rem);
+    font-weight: bolder;
+    text-transform: uppercase;
+    padding: 1rem 1.5rem;
+    border-radius: 3.5rem;
+    box-shadow: 0 .5rem 1rem rgba(0,0,0,.15)!important;
+  }
+
+  .image {
+    background-image:linear-gradient(rgba(231, 44, 112, 0.5), rgba(130, 209, 228, 0.645)), url(../../src/assets/sint_2022.jpg);
+    background-size: cover;
+    height: 81vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
 </style>
